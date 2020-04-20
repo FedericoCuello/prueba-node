3f1ab81653d172997d43be54b9eaaa13a194aa1c
FedericoCuello <cuellofederico94@gmail.com>
2020-04-20 16:07:43 -0300

    Mi primer repositorio en Git Hub

---
+++ practicanoche.js
@@ -0,0 +1,7 @@
+let destinosDelMundo = ['Marruecos', 'Bariloche', 'Barcelona', 'China', 'Grecia']
+
+let [Bariloche] = destinosDelMundo
+let [China] = destinosDelMundo
+
+console.log([,Bariloche,,,])
+console.log([,,,China,]);
